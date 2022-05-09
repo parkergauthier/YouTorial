426cc6b1395af6404c3fe02fd445d09a3615c053
Kevin O'Connor <96597458+k-oconnor@users.noreply.github.com>
2022-05-09 18:06:57 -0500

    Youtube comments to SQL database

--- scraping/loop.py
+++ scraping/loop.py
@@ -1,18 +1,28 @@
+
 import os
+from matplotlib.cbook import flatten
 import googleapiclient.discovery
 import pandas as pd
 import json
+from sqlalchemy import create_engine
 
 from sqlalchemy import values
 from metrics import get_metrics, extract_metrics
 from random import sample
 from comments_request import get_comments, clean_comments
 
+# Connecting to DB
+conn_string = "postgresql://youtube-project:Zhanghaokun_6@35.226.197.36/youtube-content"
+db = create_engine(conn_string)
+conn = db.connect()
+
+
 
 # Setting Paths
 API_BASE_DIR = "scraping"
 API_KEY_PATH = os.path.join(API_BASE_DIR, "api_keys.json")
 JSON_PATH_IN = os.path.join(API_BASE_DIR, "500_videos.json")
+OUT_PATH_TEST = os.path.join(API_BASE_DIR,"testing.csv")
 
 # Reading API Key
 with open(API_KEY_PATH, "r") as f:
@@ -31,19 +41,15 @@ videos_list = ["SwSbnmqk3zY", videos['videoID']['1'],
 list_comments = []
 list_metrics = []
 
-
 comment_dict = {}
 for i in videos_list:
     comments_dicts = get_comments(i, apiKey=api_key)['items']
     clean_comments_list = clean_comments(comments_dicts)
-    dict_comments = {
-        "videoId": i,
-        "comments": clean_comments_list
-    }
-    list_comments += [dict_comments]
-    metrics_dicts = get_metrics(i, apiKey=api_key)
-    extract_metrics_list = extract_metrics(metrics_dicts)
-    list_metrics += [extract_metrics_list]
-    comment_dict['videoId']= list_comments
-    my_big_fat_df= pd.DataFrame(comment_dict['videoId'])
-print(my_big_fat_df)
+
+    video = i
+
+    video_string = [video]*len(clean_comments_list)
+
+    df = pd.DataFrame(list(zip(video_string, clean_comments_list)), columns=["videoId", "comment"])
+    df.to_sql(con=conn, name="youtube_comment", if_exists="append")
+
