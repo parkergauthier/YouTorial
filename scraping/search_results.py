b0e6836e5de6b82d0fbccaf2f4de57ecd547978d
legitscrub <jobowman@utexas.edu>
2022-05-10 01:00:05 -0500

    no friends

--- scraping/search_results.py
+++ scraping/search_results.py
@@ -19,7 +19,7 @@ KEY_PATH = os.path.join(BASE_DIR, "api_keys.json")
 
 with open(KEY_PATH) as f:
     all_keys = json.load(f)
-    api_key = all_keys['Karlo_key']
+    api_key = all_keys['potatacago_key']
 #api_key = 'AIzaSyDjCXavvnwba1KARYeX0z-FhiVlf6bnzcg'
 
 
@@ -41,7 +41,7 @@ def request_search_results(token='', region_center='31.898608,-103.346556'):
         maxResults=50,
         topicId="/m/032tl | /m/01k8wb | /m/027x7n | /m/02wbm",
         pageToken=token,
-        q="draw",
+        q="make firends",
         type="video",
         order="viewCount",
         videoCategoryId="26",
@@ -161,6 +161,6 @@ def get_tutorial_url_list(loop_len=50, track=True):
 
 
 if __name__ == "__main__":
-    num_iterations = 3
+    num_iterations = 4
     full_vid_list = get_tutorial_url_list(num_iterations, track=True)
     print(full_vid_list)
