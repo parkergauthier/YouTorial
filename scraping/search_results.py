85dd9e03dab849f00620c1b553ed550119f0f7a6
amal-kadri <98130059+amal-kadri@users.noreply.github.com>
2022-05-09 20:40:38 -0500

    updating search query

--- scraping/search_results.py
+++ scraping/search_results.py
@@ -41,7 +41,7 @@ def request_search_results(token='', region_center='31.898608,-103.346556'):
         maxResults=50,
         topicId="/m/032tl | /m/01k8wb | /m/027x7n | /m/02wbm",
         pageToken=token,
-        q="python | coding | NLP | text analysis | tutorial",
+        q="python | coding",
         type="video",
         videoCategoryId="26",
         regionCode='US',
