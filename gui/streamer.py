7a6ae206c5fee011cb213a39f8171d4496ed6eb8
akemp18 <51224047+akemp18@users.noreply.github.com>
2022-05-10 15:51:41 -0500

    streamlit code updated

--- gui/streamer.py
+++ gui/streamer.py
@@ -3,37 +3,36 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from streamlit_player import st_player
-from streamlit_player import st_player, _SUPPORTED_EVENTS
-from streamlit_gallery.utils.readme import readme
 
+def main():
+    base_url = "https://www.youtube.com/watch?v="
 
-base_url = "https://www.youtube.com/watch?v="
+# SQL query once sentiment analysis is done...
 
 # get top 5 ids of ranked results 
-query_ids = """
-    select top 5 VideoID
-    from youtube_ID    
-"""
-
-get_urls(query_ids):
-    url1 = base_url + f"{query_ids[0]}"
-    url2 = base_url + f"{query_ids[1]}"
-    url3 = base_url + f"{query_ids[2]}"
-    url4 = base_url + f"{query_ids[3]}"
-    url5 = base_url + f"{query_ids[4]}"
-    return(list(url1, url2, url3, url4, url5))
-
-with st.container():
+# query_ids = """
+#     select top 5 VideoID
+#     from youtube_ID    
+# """
+# needs to reutn below list of ids: 
+    query_ids = ["ZJy1ajvMU1k", "LdE9u0-SwEY", "ITP6uA8AFto", "yOgQIEywEWg", "Fo9EbhLWW1s"]
+    # paste video IDs with base youtube url 
+    def get_urls(query_ids):
+        urls = []
+        for i in range(0,4):
+            url = base_url + f"{query_ids[i]}"
+            urls.append(url)
+        return urls
     
-def main():
+    top_urls = get_urls(query_ids)
+
     st.title("YouTorial")
-    st.write("Based on our algorithm, these are your top five recommended tutorials")
+    st.write("Based on our algorithm, these are your top five recommended tutorials:")
 
-    col1, col2, col3, col4, col5 = st.beta_columns(5)
-    # Embed a youtube video
-    for (i in get_urls(query_ids)): 
-        st_player(i)
-
+    with st.container(): 
+    # Embed top 5 youtube videos 
+        for i in range(0,4):
+            st_player(top_urls[i])
 
 # to run: streamlit run gui/streamer.py
 if __name__ == "__main__":
