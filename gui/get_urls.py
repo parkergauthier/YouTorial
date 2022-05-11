1858ed20d9b791562a7f92a7ce4c557edbe51ec4
akemp18 <51224047+akemp18@users.noreply.github.com>
2022-05-10 20:22:44 -0500

    updated streamlit code

--- gui/get_urls.py
+++ gui/get_urls.py
@@ -1,6 +1,3 @@
 import pandas as pd
 import os
 
-
-if __name__ == "__main__":
-    get_urls()
--- gui/streamer.py
+++ gui/streamer.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 from streamlit_player import st_player
 
+
 def main():
     base_url = "https://www.youtube.com/watch?v="
 
@@ -19,20 +20,37 @@ def main():
     # paste video IDs with base youtube url 
     def get_urls(query_ids):
         urls = []
-        for i in range(0,4):
+        for i in range(0,5):
             url = base_url + f"{query_ids[i]}"
             urls.append(url)
         return urls
     
     top_urls = get_urls(query_ids)
+    st.set_page_config(layout="wide")
+    st.image('gui/youtorial.png')
+    st.subheader("Based on our algorithm, these are your top five recommended tutorials:")
 
-    st.title("YouTorial")
-    st.write("Based on our algorithm, these are your top five recommended tutorials:")
-
-    with st.container(): 
+    
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col1:
+        event = st_player(top_urls[0])
+        st.write(event)
+    with col2:
+        event = st_player(top_urls[1])
+        st.write(event)
+    with col3:
+        event = st_player(top_urls[2])
+        st.write(event) 
+    with col4:
+        event = st_player(top_urls[3])
+        st.write(event)   
+    with col5:
+        event = st_player(top_urls[4])
+        st.write(event)
+ 
     # Embed top 5 youtube videos 
-        for i in range(0,4):
-            st_player(top_urls[i])
+        # for i in range(0,5):
+        #     st_player(top_urls[i])
 
 # to run: streamlit run gui/streamer.py
 if __name__ == "__main__":
