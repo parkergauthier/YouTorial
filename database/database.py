0edc03f994ba2b4fee12c1f086fcf07514682b6b
Kevin O'Connor <96597458+k-oconnor@users.noreply.github.com>
2022-05-12 13:01:48 -0500

    Added .env and database script

---
+++ database/database.py
@@ -0,0 +1,16 @@
+
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+
+load_dotenv()
+
+DATABASE_USERNAME = os.environ["DATABASE_USERNAME"]
+DATABASE_PASSWORD = os.environ["DATABASE_PASSWORD"]
+DATABASE_HOST = os.environ["DATABASE_HOST"]
+DATABASE_PORT = os.environ["DATABASE_PORT"]
+DATABASE_DATABASE = os.environ["DATABASE_DATABASE"]
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_DATABASE}"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
