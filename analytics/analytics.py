7aa1a9502e98f08864311f1b734465a38870878e
fyx0199 <feng2520y@gmail.com>
2022-05-12 01:43:44 -0500

    finish corr, distri

--- analytics/analytics.py
+++ analytics/analytics.py
@@ -34,9 +34,6 @@ data_analytics = pd.DataFrame(data_analytics, columns=['videoID','views_count','
 # df = pd.DataFrame(my_list, columns = ['Names'])
 print(data_analytics)
 
-# show the correlation
-data_analytics.corr()
-
 
 # Videos views according to their Likes and Comments
 plt.figure(figsize=(16,8))
@@ -47,6 +44,13 @@ plt.ylabel('Comments', fontsize=15)
 likes = data_analytics["likes"].values
 comments_ = data_analytics["comments_"].values
 views = data_analytics["views_count"].values
+likes_ratio = data_analytics["like_ratios"].values
+comment_ratio = data_analytics['comment_ratio'].values
+length = data_analytics['length_'].values
+polarity = data_analytics['polarity'].values
+subjectivity = data_analytics['subjectivity'].values
+
+
 
 plt.scatter(likes, comments_, s = views.astype(float) / 20000, edgecolors='white')
 plt.show()
@@ -61,6 +65,15 @@ data_analytics.replace([np.inf, -np.inf, np.nan], 0, inplace=True)
 plt.figure(figsize = (12,6))
 
 
+# show the correlation
+plt.figure(figsize = (12,8))
+
+# correlation analysis
+sns.heatmap(data_analytics[['views_log','likes_log', 'comment_log', 
+                            'polarity', 'subjectivity']].corr(), annot=True)
+plt.show()
+
+
 # plot views-like
 fig, ax = plt.subplots()
 _ = plt.scatter(x=data_analytics['views_count'], y=data_analytics['likes'], color='#ffa600', edgecolors="#000000", linewidths=0.5)
@@ -73,28 +86,49 @@ _ = plt.scatter(x=data_analytics['views_count'], y=data_analytics['comments_'], 
 _ = ax.set(xlabel="Views", ylabel="Comments")
 _ = ax.set_title('Views - Comments Scatter Plot')
 
+# plot views-polarity
+fig, ax = plt.subplots()
+_ = plt.scatter(x=data_analytics['views_count'], y=data_analytics['polarity'], color='#87CEFA', edgecolors="#000000", linewidths=0.5)
+_ = ax.set(xlabel="Views", ylabel="Polarity")
+_ = ax.set_title('Views - Polarity Scatter Plot')
+
+# plot views-subjectivity
+fig, ax = plt.subplots()
+_ = plt.scatter(x=data_analytics['views_count'], y=data_analytics['subjectivity'], color='#B0C4DE', edgecolors="#000000", linewidths=0.5)
+_ = ax.set(xlabel="Views", ylabel="Subjectivity")
+_ = ax.set_title('Views - Subjectivity Scatter Plot')
+
+
 # plot likes-comments
 fig, ax = plt.subplots()
 _ = plt.scatter(x=data_analytics['likes'], y=data_analytics['comments_'], color='tomato', edgecolors="#000000", linewidths=0.5)
 _ = ax.set(xlabel="Likes", ylabel="Comments")
 _ = ax.set_title('Likes - Comments Scatter Plot')
 
+# # plot length - likes
+# fig, ax = plt.subplots()
+# _ = plt.scatter(x=data_analytics['views_count'], y=data_analytics['length_'], color='#000080', edgecolors="#000000", linewidths=0.5)
+# _ = ax.set(xlabel="Views", ylabel="Length")
+# _ = ax.set_title('Views - Length Scatter Plot')
+
+
 # views log distribution
 plt.show()
 g1 = sns.distplot(data_analytics['views_log'])
 g1.set_title("VIEWS LOG DISTRIBUITION", fontsize=16)
 
-# # polarity distribution
-# plt.show()
-# g2 = sns.distplot(data_analytics['polarity'].astype(int),color='green')
-# g2.set_title('Polarity DISTRIBUITION', fontsize=16)
+plt.show()
+g2 = sns.distplot(data_analytics['likes_log'])
+g2.set_title("LIKES LOG DISTRIBUITION", fontsize=16)
 
+plt.show()
+g3 = sns.distplot(data_analytics['comment_log'])
+g3.set_title("COMMET LOG DISTRIBUITION", fontsize=16)
 
-# # subjectivity dirstribution
-# plt.show()
-# g4 = sns.distplot(data_analytics['comment_log'])
-# g4.set_title("COMMENTS LOG DISTRIBUITION", fontsize=16)
+plt.show()
+g4 = sns.distplot(data_analytics['polarity'])
+g4.set_title("POLARITY DISTRIBUITION", fontsize=16)
 
-# plt.subplots_adjust(wspace = 0.2, hspace = 0.4,top = 0.9)
-
-
+plt.show()
+g5 = sns.distplot(data_analytics['subjectivity'])
+g5.set_title("SUBEJECTIVITY DISTRIBUITION", fontsize=16)
