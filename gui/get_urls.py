f8e261fa59f3ac800edcc5b706334cf7502dcb96
amal-kadri <98130059+amal-kadri@users.noreply.github.com>
2022-05-11 20:52:02 -0500

    get top 6
    Co-Authored-By: bowman-jonathan <98063426+bowman-jonathan@users.noreply.github.com>
    Co-Authored-By: Parker Gauthier <97997086+parkergauthier@users.noreply.github.com>
    Co-Authored-By: KarloVlahek <91391943+KarloVlahek@users.noreply.github.com>
    Co-Authored-By: Alice Kemp <98052656+alicekemp@users.noreply.github.com>

--- gui/get_urls.py
+++ gui/get_urls.py
@@ -1,3 +1,64 @@
 import pandas as pd
-import os
+import sqlalchemy
+from sqlalchemy import create_engine, text
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+conn_string = "postgresql://youtube-project:Zhanghaokun_6@35.226.197.36/youtube-content"
+engine = sqlalchemy.create_engine(conn_string)
 
+
+def clean_search_input(input_str):
+    clean_search = input_str.lower().replace(' ', '&')
+    return clean_search
+
+
+def define_query(search_input):
+    bigger_query = f"""select
+	total_vids."videoID",
+	complete_videos.title,
+	complete_videos."channelID",
+	total_vids.views_count,
+	total_vids.likes,
+	total_vids.comments_,
+	total_vids.length_,
+	total_vids.like_ratios,
+	total_vids.comment_ratio,
+	total_vids.polarity,
+	total_vids.subjectivity
+	from
+		total_vids
+	left join complete_videos on
+		total_vids."videoID" = complete_videos."videoID"
+	where total_vids."videoID" in (
+	select "videoID" from complete_videos 
+	where to_tsvector(title) @@ to_tsquery('{clean_search_input(search_input)}'))"""
+    return bigger_query
+
+
+def get_top_six(input_query):
+    input_query = 'How to play the Guitar'
+    sql_query = define_query(input_query)
+
+    stats_table = pd.read_sql(text(sql_query), con=engine)
+    features = ['views_count', 'likes', 'comments_',
+                'like_ratios', 'comment_ratio', 'polarity', 'subjectivity']
+    id_frame = ['videoID', 'title', 'channelID']
+    # Separating out the features
+    x = stats_table.loc[:, features].values
+    # Separating out the target
+    y = stats_table.loc[:, id_frame].values
+    # Standardizing the features
+    x = StandardScaler().fit_transform(x)
+    pca = PCA(n_components=1)
+    principalComponents = pca.fit_transform(x)
+    principalDf = pd.DataFrame(data=principalComponents, columns=['PC1'])
+    finalDf = pd.concat([stats_table[['videoID', 'title', 'channelID']],
+                        principalDf], axis=1).sort_values(by='PC1', ascending=False)
+    id_list = finalDf['videoID'].head(6).to_list()
+    return id_list
+
+
+if __name__ == "__main__":
+    search_string = "How to draw Sonic the Hedgehog"
+    results = get_top_six(search_string)
+    print(results)
