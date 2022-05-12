b0eac338ed60f72f02c73e40218c1c2fb134b4da
legitscrub <jobowman@utexas.edu>
2022-05-12 15:59:41 -0500

    database repo deep clean

--- database/tables_and_views/2_create_view_unique_id.sql
+++ database/tables_and_views/2_create_view_unique_id.sql
@@ -1,4 +1,5 @@
 -- use to filter duplicate videos from youtube_id for subsequent views
+-- use count function to count total number of unique videos
 create view unique_id as
 select distinct("videoID")
 from youtube_id
--- database/tables_and_views/6_create_view_no_analysis.sql
+++ database/tables_and_views/6_create_view_no_analysis.sql
@@ -1,4 +1,5 @@
 -- use as query table to feed videos from youtube_metrics and youtube_comments into analytics
+-- use count function to count number of videos with data yet to be analyzed
 CREATE view no_analysis AS 
 SELECT *
 FROM youtube_metrics
