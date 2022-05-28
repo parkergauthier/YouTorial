51ce4c2b1b49121f61b46b07b026085c3e148834
parkergauthier <97997086+parkergauthier@users.noreply.github.com>
2022-05-28 14:23:01 -0500

    credential cleanup

--- scraping/1_search_results.py
+++ scraping/1_search_results.py
@@ -1,4 +1,3 @@
-
 # -*- coding: utf-8 -*-
 
 # Sample Python code for youtube.search.list
@@ -14,18 +13,17 @@ import pandas as pd
 import json
 from database import engine
 
-BASE_DIR = 'scraping'
+BASE_DIR = "scraping"
 KEY_PATH = os.path.join(BASE_DIR, "file_dependencies/demo_api_keys.json")
 TOKEN_PATH = os.path.join(BASE_DIR, "file_dependencies/tokens_file.json")
 
 with open(KEY_PATH) as f:
     all_keys = json.load(f)
-    api_key = all_keys['Key1']
-#api_key = 'AIzaSyDjCXavvnwba1KARYeX0z-FhiVlf6bnzcg'
+    api_key = all_keys["Key1"]
 
 
-def request_search_results(query, token='', region_center='31.898608,-103.346556'):
-    '''requests 50 search results for a specified query using the Youtube Data API V3, returns a complex dictionary'''
+def request_search_results(query, token="", region_center="31.898608,-103.346556"):
+    """requests 50 search results for a specified query using the Youtube Data API V3, returns a complex dictionary"""
     # Disable OAuthlib's HTTPS verification when running locally.
     # *DO NOT* leave this option enabled in production.
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
@@ -35,7 +33,8 @@ def request_search_results(query, token='', region_center='31.898608,-103.346556
     DEVELOPER_KEY = api_key
 
     youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, developerKey=DEVELOPER_KEY)
+        api_service_name, api_version, developerKey=DEVELOPER_KEY
+    )
     try:
         request = youtube.search().list(
             part="snippet",
@@ -46,16 +45,22 @@ def request_search_results(query, token='', region_center='31.898608,-103.346556
             type="video",
             order="viewCount",
             videoCategoryId="26",
-            regionCode='US',
+            regionCode="US",
             location=region_center,
-            locationRadius="1000km"
+            locationRadius="1000km",
         )
         response = request.execute()
     except googleapiclient.errors.HttpError as API_ERROR:
-        print("==*==*==*==*==*==*==*==*==*==*==*==*==*==*==*==*==*==*==*==*==*==*==*==*")
+        print(
+            "==*==*==*==*==*==*==*==*==*==*==*==*==*==*==*==*==*==*==*==*==*==*==*==*"
+        )
         print(API_ERROR)
-        print("ERROR: Request could not be processed. Check to see if your API key has met it's quota")
-        print("==*==*==*==*==*==*==*==*==*==*==*==*==*==*==*==*==*==*==*==*==*==*==*==*")
+        print(
+            "ERROR: Request could not be processed. Check to see if your API key has met it's quota"
+        )
+        print(
+            "==*==*==*==*==*==*==*==*==*==*==*==*==*==*==*==*==*==*==*==*==*==*==*==*"
+        )
         quit()
 
     return response
@@ -67,50 +72,55 @@ def get_vid_ids(dict_list):
 
     for i in range(len(dict_list)):
         vid_info = {
-            'videoID': dict_list[i]['id']['videoId'],
-            'title': dict_list[i]['snippet']['title'],
-            'channelID': dict_list[i]['snippet']['channelId']
+            "videoID": dict_list[i]["id"]["videoId"],
+            "title": dict_list[i]["snippet"]["title"],
+            "channelID": dict_list[i]["snippet"]["channelId"],
         }
         id_list.append(vid_info)
     return id_list
 
 
 def get_tutorial_url_list(query, loop_len=50, track=True):
-    '''Calls functions to request youtube search results as a list of video IDs and titles as a dictionary, with 50 IDs per iteration'''
+    """Calls functions to request youtube search results as a list of video IDs and titles as a dictionary, with 50 IDs per iteration"""
     full_id_list = []
 
     # define region centers:
-    west = '41.564718,-116.336400'
-    texas = '31.898608,-103.346556'
-    midwest = '46.095507,-94.581880'
-    southeast = '33.494011,-84.344213'
-    northeast = '44.925322,-71.730317'
+    west = "41.564718,-116.336400"
+    texas = "31.898608,-103.346556"
+    midwest = "46.095507,-94.581880"
+    southeast = "33.494011,-84.344213"
+    northeast = "44.925322,-71.730317"
 
     print("Number of results so far:")
 
     for i in range(loop_len):
-        with open(TOKEN_PATH, 'r') as json_file:
+        with open(TOKEN_PATH, "r") as json_file:
             token_dict = json.load(json_file)
 
-        west_soup = request_search_results(query,
-                                           token=token_dict['west_page'], region_center=west)
-        west_list = west_soup['items']
+        west_soup = request_search_results(
+            query, token=token_dict["west_page"], region_center=west
+        )
+        west_list = west_soup["items"]
 
-        texas_soup = request_search_results(query,
-                                            token=token_dict['texas_page'], region_center=texas)
-        texas_list = texas_soup['items']
+        texas_soup = request_search_results(
+            query, token=token_dict["texas_page"], region_center=texas
+        )
+        texas_list = texas_soup["items"]
 
-        midwest_soup = request_search_results(query,
-                                              token=token_dict['midwest_page'], region_center=midwest)
-        midwest_list = midwest_soup['items']
+        midwest_soup = request_search_results(
+            query, token=token_dict["midwest_page"], region_center=midwest
+        )
+        midwest_list = midwest_soup["items"]
 
-        southeast_soup = request_search_results(query,
-                                                token=token_dict['southeast_page'], region_center=southeast)
-        southeast_list = southeast_soup['items']
+        southeast_soup = request_search_results(
+            query, token=token_dict["southeast_page"], region_center=southeast
+        )
+        southeast_list = southeast_soup["items"]
 
-        northeast_soup = request_search_results(query,
-                                                token=token_dict['northeast_page'], region_center=northeast)
-        northeast_list = northeast_soup['items']
+        northeast_soup = request_search_results(
+            query, token=token_dict["northeast_page"], region_center=northeast
+        )
+        northeast_list = northeast_soup["items"]
 
         full_id_list += get_vid_ids(west_list)
         full_id_list += get_vid_ids(texas_list)
@@ -120,24 +130,23 @@ def get_tutorial_url_list(query, loop_len=50, track=True):
 
         # transform list to dataframe
 
-        df = pd.DataFrame(
-            full_id_list).drop_duplicates().set_index(['videoID'])
+        df = pd.DataFrame(full_id_list).drop_duplicates().set_index(["videoID"])
         # print(df.shape)
 
         # upload dataframe to table
         df.to_sql(con=engine, name="youtube_id", if_exists="append")
 
         print("=========================================")
-        num_results = (i*250+250)
+        num_results = i * 250 + 250
         if track:
             print(f"{num_results} so far!")
         # redefine tokens dict
         try:
-            token_dict['west_page'] = west_soup['nextPageToken']
-            token_dict['texas_page'] = texas_soup['nextPageToken']
-            token_dict['midwest_page'] = midwest_soup['nextPageToken']
-            token_dict['southeast_page'] = southeast_soup['nextPageToken']
-            token_dict['northeast_page'] = northeast_soup['nextPageToken']
+            token_dict["west_page"] = west_soup["nextPageToken"]
+            token_dict["texas_page"] = texas_soup["nextPageToken"]
+            token_dict["midwest_page"] = midwest_soup["nextPageToken"]
+            token_dict["southeast_page"] = southeast_soup["nextPageToken"]
+            token_dict["northeast_page"] = northeast_soup["nextPageToken"]
             print(token_dict)
         except:
             token_dict = {
@@ -145,14 +154,14 @@ def get_tutorial_url_list(query, loop_len=50, track=True):
                 "texas_page": "",
                 "midwest_page": "",
                 "southeast_page": "",
-                "northeast_page": ""
+                "northeast_page": "",
             }
-            with open(TOKEN_PATH, 'w+') as json_file:
+            with open(TOKEN_PATH, "w+") as json_file:
                 json.dump(token_dict, json_file)
             print("You have reached the end of Search Results for this Query :D")
             break
 
-        with open('scraping/file_dependencies/tokens_file.json', 'w+') as json_file:
+        with open("scraping/file_dependencies/tokens_file.json", "w+") as json_file:
             json.dump(token_dict, json_file)
 
     return full_id_list
@@ -160,5 +169,5 @@ def get_tutorial_url_list(query, loop_len=50, track=True):
 
 if __name__ == "__main__":
     num_iterations = 1
-    query = 'Teach & Python | C++'
+    query = "Teach & Python | C++"
     full_vid_list = get_tutorial_url_list(query, num_iterations, track=True)
