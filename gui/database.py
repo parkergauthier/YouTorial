2c6eed89dc577e72b56edd688d4ff91f179da176
Parker Gauthier <pgauthier@utexas.edu>
2022-05-29 23:15:08 -0500

    deployment

--- gui/database.py
+++ gui/database.py
@@ -1,17 +1,24 @@
-
 import os
+import toml
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 import psycopg2
 
 load_dotenv()
 
-DATABASE_USERNAME = os.environ["DATABASE_USERNAME"]
-DATABASE_PASSWORD = os.environ["DATABASE_PASSWORD"]
-DATABASE_HOST = os.environ["DATABASE_HOST"]
-DATABASE_PORT = os.environ["DATABASE_PORT"]
-DATABASE_DATABASE = os.environ["DATABASE_DATABASE"]
+toml_data = toml.load(".streamlit/secrets.toml")
 
+# DATABASE_USERNAME = os.environ["DATABASE_USERNAME"]
+# DATABASE_PASSWORD = os.environ["DATABASE_PASSWORD"]
+# DATABASE_HOST = os.environ["DATABASE_HOST"]
+# DATABASE_PORT = os.environ["DATABASE_PORT"]
+# DATABASE_DATABASE = os.environ["DATABASE_DATABASE"]
+
+DATABASE_USERNAME = toml_data["sql"]["DATABASE_USERNAME"]
+DATABASE_PASSWORD = toml_data["sql"]["DATABASE_PASSWORD"]
+DATABASE_HOST = toml_data["sql"]["DATABASE_HOST"]
+DATABASE_PORT = toml_data["sql"]["DATABASE_PORT"]
+DATABASE_DATABASE = toml_data["sql"]["DATABASE_DATABASE"]
 SQLALCHEMY_DATABASE_URL = f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_DATABASE}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
